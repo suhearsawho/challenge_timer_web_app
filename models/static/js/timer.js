08d3166badb7fe9c7697cac309b4a2606757555a
Susan Su <susansu.software@gmail.com>
2019-03-20 12:13:30 -0700

    working buttons (5 minute, 15 minute and complete)

--- models/static/js/timer.js
+++ models/static/js/timer.js
@@ -1,26 +1,52 @@
 /* Declare Variables */
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
+var on_off = document.getElementsByClassName("start_pause");
+var time_left = parseInt(document.getElementById("user_time").innerHTML, 10) * 60;
+var timer_clock;
 
-/* Timer Function */
-function countdown(time) {
-	let time_left = time * 60;
-	let timer_clock = setInterval(inCountdown, 1000);
-	function inCountdown() {
-		let minutes_value = Math.floor((time_left / 60));
-		let seconds_value = Math.floor((time_left % 60));
-		
-		if (seconds_value < 10)
-			seconds_value = '0' + seconds_value;
-		minutes.innerHTML = minutes_value;
-		seconds.innerHTML = seconds_value;
-		if (time_left == 0)
-		{
-			clearInterval(timer_clock);
-		}
-		else
-		{
-			time_left = time_left - 1;
-		}
+
+function countdown() {
+	let str1 = "start";
+	let str2 = "pause";
+	if (on_off[0].id === str1)
+	{
+		document.getElementById("start").innerHTML = 'Pause';
+		document.getElementById("start").id = "pause";
+		timer_clock = setInterval(inCountdown, 1000);
 	}
+	else
+	{
+		document.getElementById("pause").innerHTML = 'Begin';
+		document.getElementById("pause").id = "start";
+		clearInterval(timer_clock);
+	}	
+}
+
+function inCountdown() {
+	let minutes_value = Math.floor((time_left / 60));
+	let seconds_value = Math.floor((time_left % 60));
+	
+	if (seconds_value < 10)
+		seconds_value = '0' + seconds_value;
+	minutes.innerHTML = minutes_value;
+	seconds.innerHTML = seconds_value;
+	if (time_left == 0)
+	{
+		clearInterval(timer_clock);
+	}
+	else
+	{
+		time_left = time_left - 1;
+	}
+}
+
+function add_time(value) {
+	clearInterval(timer_clock);
+	time_left = time_left + (value * 60);
+	timer_clock = setInterval(inCountdown, 1000);
+}
+
+function finish() {
+	clearInterval(timer_clock);
 }
